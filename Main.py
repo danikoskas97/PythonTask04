b3cdb007cba634651fc6c3fc949193e8083d40d6
dani koskas <koskas.dani@gmail.com>
2020-05-03 12:53:18 +0200

    abcd

--- Main.py
+++ Main.py
@@ -1,10 +1,10 @@
 import datetime
-
+from person import Person
 from lecturer import Lecturer
 from student import Student
 
-student1 = Student("yoad", "gadot", 1233, "Tel-aviv", ("Python", "Java"), 2020)
-student2 = Student("Dani", "Koskas", 2313, "Raanana", ("Python", "advanced algorithm"), 2020)
-lecturer1 = Lecturer("gab", "rich", 43600, "Eilat", ("Python", "advanced algorithm"), 2020)
+student1 = Student(firstName="yoad", lastName="gadot", id=1233, adress="Tel-aviv", courses=("Python", "Java"), year=2020)
+student2 = Student(firstName="Dani", lastName="Koskas", id=2313, adress="Raanana", courses=("Python", "advanced algorithm"), year=2020)
+lecturer1 = Lecturer(firstName="gab", lastName="rich", id=43600, adress="Eilat", rate_for_hour=200, year=2020)
 
 student2.get_name()
